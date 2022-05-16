f4d55119fc664838ba0733ce7cb0a6028fdfd78f
Asont <94078125+Asont@users.noreply.github.com>
2022-05-16 08:44:50 +0300

    Homework 9 in progress

--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -18,7 +18,14 @@ test('sort name up', () => {
     const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
 
     console.log(newState)
-  //  expect(...).toBe(...)
+    expect([
+        {_id: 0, name: 'Кот', age: 3},
+        {_id: 2, name: 'Коля', age: 16},
+        {_id: 4, name: 'Дмитрий', age: 40},
+        {_id: 3, name: 'Виктор', age: 44},
+        {_id: 5, name: 'Ирина', age: 55},
+        {_id: 1, name: 'Александр', age: 66},
+    ]).toBe(newState)
 })
 test('sort name down', () => {
     const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
